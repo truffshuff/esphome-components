47993ca983048a46c835e830fad5735754462df5
truffshuff <dustinh@alumni.psu.edu>
2025-11-10 09:24:17 -0500

    Refactor weather_helpers initialization: add dependencies and schema registration

--- components/weather_helpers/__init__.py
+++ components/weather_helpers/__init__.py
@@ -1,3 +1,12 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+
+DEPENDENCIES = []
 
 weather_helpers_ns = cg.esphome_ns.namespace("weather_helpers")
+
+# Register a dummy component (optional if you want it to appear in logs)
+CONFIG_SCHEMA = cv.Schema({})
+
+async def to_code(config):
+    pass
